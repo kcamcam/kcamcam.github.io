b56ef13d4379660d1961511ee78ba9ad69ea4ac9
Kevin Camellini <kevincamellini@gmail.com>
2018-08-23 02:47:50 -0400

    ffs

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
+// @codekit-prepend 'data.js'
+// @codekit-prepend 'templates.js'
 // @codekit-prepend 'jquery.js'
 // @codekit-prepend 'materialize.js'
 // @codekit-prepend 'font-awesome.js'
-// @codekit-prepend 'data.js'
-// @codekit-prepend 'templates.js'
+// @codekit-prepend 'analytics.js'
